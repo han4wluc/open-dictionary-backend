99f757a3e940871ca6b6faf523906af0a620a746
han4wluc <han4wluc@gmail.com>
2017-11-25 18:50:43 +0800

    cors

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 var port = 3000;
 var postGraphQLLink = `postgresql://postgres:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/postgres`;
 
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
+
 app.use(postgraphql(postGraphQLLink, "public", {
   graphiql: true,
   watchPg: true,
