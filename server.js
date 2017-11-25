46511fcf246871ce259c440c818b5fea4325f295
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2017-11-25 13:13:29 +0800

    new link, deleted route

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const postgraphql = require('postgraphql').postgraphql;
 
 const app = express();
-app.use('/', routes);
+// app.use('/', routes);
 
 var port = 3000;
-var postGraphQLLink = 'postgres://localhost:5432';
+var postGraphQLLink = 'postgresql://postgres:open-dictionary-open-dictionary@159.89.206.252:5432/postgres';
 
 app.use(postgraphql(postGraphQLLink));
 app.listen(port, ()=>{
